06b2e86303749357ca94955dc82b16435f9c5ea7
lucas.vmub1804 <luyuexin93@outlook.com>
2018-05-11 17:02:22 +0800

    from pycharm

--- mytest/bulid.py
+++ mytest/bulid.py
@@ -15,7 +15,7 @@ driver.find_element_by_id("su").click()
 driver.find_element_by_link_text("新闻").click()
 #driver.find_element_by_class_name("bg s_btn")
 #driver.find_element_by_tag_name()
-driver.find_element_by_xpath("//*[@id='s_tab']/a[1]").click()
+driver.find_element_by_xpath("//*[@id='s_tab']/a[1]").click() #Xpath 元素属性值
 
 sleep(5)
 driver.quit()
