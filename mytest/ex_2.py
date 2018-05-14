eab4560fcd56f13fe0b9f4d193683c40f4b476e8
lucas.vmub1804 <luyuexin93@outlook.com>
2018-05-14 16:50:13 +0800

    from pycharm browser methods update

--- mytest/ex_2.py
+++ mytest/ex_2.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
 import time
+
+#测试浏览器功能
 driver=webdriver.Firefox()
+#模拟手机浏览器大小
+print("设置浏览器宽480、高800显示")
+driver.set_window_size(480,800)
+#设置浏览器全屏显示
+#driver.maximize_window()
 
 #访问百度首页
 first_url='https://www.baidu.com'
@@ -24,4 +31,8 @@ print("forward to %s" %(second_url))
 driver.forward()
 time.sleep(3)
 
+#刷新页面
+driver.refresh()
+
+#关闭浏览器
 driver.close()
