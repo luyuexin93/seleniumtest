2648ca45650620c74e2f788836f66130282334ee
lucas.vmub1804 <luyuexin93@outlook.com>
2018-05-14 16:50:32 +0800

    from pycharm browser methods update

--- mytest/bulid.py
+++ mytest/bulid.py
@@ -17,5 +17,6 @@ driver.find_element_by_link_text("新闻").click()
 #driver.find_element_by_tag_name()
 driver.find_element_by_xpath("//*[@id='s_tab']/a[1]").click() #Xpath 元素属性值
 
+
 sleep(5)
 driver.quit()
