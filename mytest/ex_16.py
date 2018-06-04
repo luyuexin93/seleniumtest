ecd372355c6286c25983aa5e8bc6512a38be63f6
lucas.vmub1804 <luyuexin93@outlook.com>
2018-06-04 17:29:33 +0800

    from pycharm selenium test cookie

--- mytest/ex_16.py
+++ mytest/ex_16.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
-
+from time import sleep
 driver=webdriver.Firefox()
 driver.get("http://www.youdao.com")
 
+sleep(3)
+#获取所有cookie信息
 cookie = driver.get_cookies()
 name =driver.get_cookie('value')
-driver.add_cookie({'name'})
+#添加浏览器cookie
+driver.add_cookie({'name':'key-aaa','value':'value-bb'})
 print(name)
 print(cookie)
+
+for cookie in driver.get_cookies():
+    print ("%s -> %s" %(cookie['name'],cookie['value']))
 
 driver.quit()
 
